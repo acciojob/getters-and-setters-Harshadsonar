e711617e60dbe87ddaac939d659f422b85d95f59
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-07-14 17:15:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,20 +7,23 @@ class Person {
     get name() {
         return this._name;
     }
-    set age(age) {
-        this._age = age;
+	get age(){
+		return this._age;
+	}
+    set age(value) {
+        this._age = value;
     }
 }
 
 class Student extends Person {
     study() {
-        console.log(`${this._name} is studying.`);
+        console.log(`${this._name} is studying`);
     }
 }
 
 class Teacher extends Person {
     teach() {
-        console.log(`${this._age} is teaching.`);
+        console.log(`${this._name} is teaching`);
     }
 }
 
